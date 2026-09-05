b84b8dc992a3df72827bdc1ec74fc2b73947a29e
Vishal-Kumar-Shaw <vkshaw1997@gmail.com>
2026-09-05 22:34:41 +0530

    Bad Coding- Builder Pattern

---
+++ Builder-Pattern/BuilderPattern/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        User u1 = new User("Vishal","abc123","29","Banglore","vishal@test.com");
+        u1.printDetails();
+        // what if i need to use only 2 information to create a user
+        // So we need to create another constructor
+
+        // but what if we need to create user with 2 other property
+        // then we need to create some other constructor
+
+        // But what if we create a user without any constructor and add properties ac to our needs
+        // This will create another issue that is called invalid object creation
+    }
+}
---
+++ Builder-Pattern/BuilderPattern/src/User.java
@@ -0,0 +1,24 @@
+public class User {
+    private String name;
+    private String password;
+    private String age;
+    private String address;
+    private String email;
+
+    public User(String name, String password, String age, String address, String email) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+        this.address = address;
+        this.email = email;
+    }
+
+    public void printDetails() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Password: " + this.password);
+        System.out.println("Age: " + this.age);
+        System.out.println("Address: " + this.address);
+        System.out.println("Email: " + this.email);
+    }
+
+}
